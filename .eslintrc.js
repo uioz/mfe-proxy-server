3ed8843f50e06811e0ebfb94e65cca4f8c560925
ASCll <hzxbpld@163.com>
2021-04-16 16:30:30 +0800

    chore: package.json prettier eslint typescript setup

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
+};
