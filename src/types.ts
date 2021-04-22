be3591adc0f46fe8d83fad6c06ff0b2137b3a716
ASCll <hzxbpld@163.com>
2021-04-22 14:17:24 +0800

    feat: support relative path in manifest.json

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,34 @@
 export interface mfeRoute {
   domain: Array<string>;
 }
+
+export interface appInfo {
+  /**
+   * 应用程序名称(包名称)
+   */
+  name: string;
+  /**
+   * 应用程序所在目录
+   */
+  dir: string;
+  /**
+   * 路由配置文件地址
+   */
+  routePath: string;
+  /**
+   * 构建后的输出路径
+   */
+  outputDir: string;
+  /**
+   * 静态资源路径
+   */
+  staticDir: string;
+}
+
+export interface manifest {
+  // TODO: support in next major version
+  env: {
+    [key: string]: string;
+  };
+  applications: Array<appInfo>;
+}
