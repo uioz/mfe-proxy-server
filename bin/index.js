07961ab34c3a50efc8f1b45e1efb2eb11ad62870
ASCll <hzxbpld@163.com>
2021-04-22 15:55:25 +0800

    feat: 在弹出 mfe-server.js 前进行检查

--- bin/index.js
+++ bin/index.js
@@ -10,11 +10,27 @@ const projectPath = resolve(CWD, '../../');
 
 const TEMPLATE_NAME = 'template.js';
 const TARGET_NAME = 'mfe-server.js';
+const PACKAGE_NAME = require('../package.json').name;
 
 async function run() {
   const template = await readFile(join(CWD, 'bin', TEMPLATE_NAME));
 
-  await writeFile(join(projectPath, TARGET_NAME), template);
+  try {
+    await writeFile(join(projectPath, TARGET_NAME), template, {
+      flag: 'wx',
+    });
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      console.log(
+        `because you already have a ${TARGET_NAME} in your project dir,so ${PACKAGE_NAME} won't eject anymore`
+      );
+      console.log(
+        `The latest template of ${TARGET_NAME} at ${__dirname}, if you have trouble with ${TARGET_NAME}`
+      );
+    } else {
+      throw error;
+    }
+  }
 }
 
 run();
