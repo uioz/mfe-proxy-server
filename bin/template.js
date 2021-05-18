415df132013670180b46f30ac28091efadb694d9
ASCll <hzxbpld@163.com>
2021-05-18 17:56:59 +0800

    fix: mistake

--- bin/template.js
+++ bin/template.js
@@ -1,32 +1,35 @@
 function generateTemplate(env) {
-  Object.assign(env, {
-    mode: 'process.env.NODE_ENV',
-    port: 80,
-    host: '0.0.0.0',
-  });
+  Object.assign(
+    {
+      port: 80,
+      host: '0.0.0.0',
+    },
+    env
+  );
 
   return `'use strict';
-  /** @type {import("fastify").FastifyInstance} */
-  const fastify = require('fastify')({ logger:true });
-  const { mfeProxyServerPlugin } = require('mfe-proxy-server');
-  
-  // DO whatever you want before mfeProxyServerPlugin
-  
-  fastify.register(mfeProxyServerPlugin, {
-    mode: ${env.mode},
-    manifest: require('./mfe-manifest.json'),
-  });
-  
-  // Run the server!
-  const start = async () => {
-    try {
-      await fastify.listen(${env.port},${env.host});
-    } catch (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-  };
-  start();`;
+/** @type {import("fastify").FastifyInstance} */
+const fastify = require('fastify')({ logger: true });
+const { mfeProxyServerPlugin } = require('mfe-proxy-server');
+
+// DO whatever you want before mfeProxyServerPlugin
+
+fastify.register(mfeProxyServerPlugin, {
+  mode: ${env.mode ? `'${env.mode}'` : 'process.env.NODE_ENV'},
+  manifest: require('./mfe-manifest.json'),
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(${env.port}, '${env.host}');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();`;
 }
 
 exports.generateTemplate = generateTemplate;
